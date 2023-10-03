cb8cbd3f6d34cfd6e13bdceba87639a83afac63a
c0a22063e0 <c0a22063e0@edu.teu.ac.jp>
2023-10-03 14:55:14 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,14 +3,23 @@ import pygame as pg
 import random
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {  # 練習３：移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
+    """ばくだん"""
     bd_img = pg.Surface((20, 20))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect() #練習１surfaceからrectを抽出する。
@@ -29,8 +38,20 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
+                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
+        """"ばくだん"""
+
         bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
-        screen.blit(bd_img, bd_rct) #練習1 
+        screen.blit(bd_img, bd_rct) #練習1 Rectを使って試しにblit
+
         pg.display.update()
         tmr += 1
         clock.tick(10)
